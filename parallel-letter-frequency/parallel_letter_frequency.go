85cf25a5e1625b4c51d16294e3a51f300037caf1
Charles Guertin <charlesguertin@live.ca>
2020-04-11 21:20:40 -0400

    Completed parallel-letter-frequency algorithm :heavy_check_mark:

---
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -0,0 +1,32 @@
+package letter
+
+// FreqMap records the frequency of each rune in a given text.
+type FreqMap map[rune]int
+
+// Frequency counts the frequency of each rune in a given text and returns this
+// data as a FreqMap.
+func Frequency(s string) FreqMap {
+	m := FreqMap{}
+	for _, r := range s {
+		m[r]++
+	}
+	return m
+}
+
+func ConcurrentFrequency(words []string) FreqMap {
+	c := make(chan FreqMap)
+	for _, word := range words {
+		go func(word string) {
+			c <- Frequency(word)
+		}(word)
+	}
+
+	result := FreqMap{}
+	for range words {
+		for letter, count := range <-c {
+			result[letter] += count
+		}
+	}
+
+	return result
+}
